f104a1ae71fc17c5e303fca92499078bfdef1cdb
Anamika Sawhney <anamika.sawhney22@gmail.com>
2022-05-27 11:57:12 +0530

    MongoDb Connectivity

---
+++ ExpressDemos/Day4/MongoDbConnectivity/app.js
@@ -0,0 +1,25 @@
+import express from "express";
+
+import connectDb from './connectDb.js';
+import {join} from "path";
+import web from './routes/web.js';
+
+const port = 3000;
+var app = express();
+
+app.use(express.json());
+const databaseUrl ="mongodb+srv://user:password123567@cluster0.ulbwxcb.mongodb.net/practiceDb?retryWrites=true&w=majority";
+
+app.use("/api", web);
+
+// const databaseUrl ="mongodb://localhost:27017";
+// app.set("views" ,"/views");
+
+app.set("view engine" ,"ejs");
+app.use(express.static(join(process.cwd(), "public")));
+
+connectDb(databaseUrl);
+app.listen(port, ()=>
+{
+  console.log(`Connected at ${port}`);
+})
